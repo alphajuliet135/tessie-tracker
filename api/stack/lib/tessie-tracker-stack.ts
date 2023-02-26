4531404ebd80f5b7ed76e837d4758c722818b682
Alexander James Cole <aj@co-le.com>
2023-02-26 13:05:17 +0100

    Transition to lambda

--- api/stack/lib/tessie-tracker-stack.ts
+++ api/stack/lib/tessie-tracker-stack.ts
@@ -3,7 +3,8 @@ import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as apigatewayv2 from 'aws-cdk-lib/aws-apigatewayv2';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 
 export class TessieTrackerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -16,10 +17,16 @@ export class TessieTrackerStack extends Stack {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
 
+    const restApi = new apigateway.RestApi(this, '', {
+      restApiName: 'tessie-tracker-api',
+    });
+
     const apiLambda = new lambda.Function(this, 'tessie-tracker-api', {
       runtime: lambda.Runtime.NODEJS_16_X,
-      code: lambda.Code.fromAsset('../.'),
+      code: lambda.Code.fromAsset('./../src'),
       handler: 'handler.handler',
     });
+
+    restApi.root.addMethod('GET', new LambdaIntegration(apiLambda, {}));
   }
 }
